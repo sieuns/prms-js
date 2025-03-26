9fa73a28347f2be728bd72618d3b85e9a1aea58f
양시은 <mn07020@gmail.com>
2025-03-26 15:23:51 +0900

    FEAT : current-asset todo 구현

--- js/components/current-asset.js
+++ js/components/current-asset.js
@@ -1,5 +1,5 @@
-import { store, updateStorage } from "../store";
-import { toHidden, toShow } from "../util";
+import { store, updateStorage } from "/js/store.js";
+import { toHidden, toShow } from "/js/util.js";
 
 const $currentAssetInput = document.querySelector(".current-asset-input");
 const $currentAssetValue = document.querySelector(".current-asset-value");
@@ -37,8 +37,6 @@ function addCurrentAssetEventListener() {
 }
 
 export function renderCurrentAsset() {
-  // TODO: 숫자에 콤마 작성
-  // TODO: currentFunds가 없는 경우
-  $currentAssetValue.textContent = store.currentFunds ?? "-";
+  $currentAssetValue.textContent = store.currentFunds?.toLocaleString() ?? "-";
   $currentAssetInput.value = store.currentFunds;
 }
