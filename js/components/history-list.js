4709a245de8b8daf8dffeb29e45f9e38f3aeb4d8
양시은 <mn07020@gmail.com>
2025-03-26 15:24:07 +0900

    FEAT : history-list todo 구현

--- js/components/history-list.js
+++ js/components/history-list.js
@@ -1,5 +1,5 @@
-import { renderCurrentAsset } from "../components/current-asset";
-import { store, removeHistory } from "../store";
+import { renderCurrentAsset } from "/js/components/current-asset.js";
+import { store, removeHistory } from "/js/store.js";
 
 const $sectionHistory = document.querySelector(".history");
 
@@ -31,46 +31,52 @@ function reRender() {
 }
 
 export function renderHistoryList() {
-  // TODO: 데이터 매핑
-  // TODO: 오름차순으로 목록 나열
-  // TODO: 항목의 시간 포맷 변경: `HH:mm`
-  // TODO: 금액 콤마 포맷 맞추기
-
   $sectionHistory.innerHTML = store.dateList
     .map(({ date, id: dateId }) => {
       const detail = store.detailList[dateId];
-      if (!detail?.length) return "";
+      if (!detail?.length) return '';
 
       return `<article class="history-per-day">
       <p class="history-date">2021년 12월 1일</p>
-      <section class="history-item">
+      ${detail
+        .sort((a, b) => b.id - a.id)
+        .map(({ description, category, amount, fundsAtThetime, createAt, id}) => {
+          const time = new Date(createAt).toLocaleTimeString("ko-kr", {
+            timeStyle : "short",
+            hourCycle : "h24"
+        });
+
+        return `<section class="history-item">
         <section class="history-item-column">
-          <div class="create-at">10:30</div>
+          <div class="create-at">${time}</div>
           <div class="history-detail">
             <div class="history-detail-row history-detail-title">
-              <p>아이스 아메리카노</p>
+              <p>${description}</p>
             </div>
             <div class="history-detail-row history-detail-subtitle">
-              <p>카페</p>
+              <p>${category}</p>
               <p>
-                1000000
+              ${amount.toLocaleString()}
                 <span>원</span>
               </p>
             </div>
           </div>
           <div class="delete-section">
-            <button class="delete-button">🗑</button>
+            <button class="delete-button" data-dateid=${dateId} data-itemid=${id}>🗑</button>
           </div>
         </section>
         <section class="history-item-caption">
           <p>
             <span>남은 자산</span>
-            <span>300000</span>
+            <span>${fundsAtThetime.toLocaleString()}</span>
             <span>원</span>
           </p>
         </section>
-      </section>
+      </section>`;
+    })
+    .join('')
+  }
+
     </article>`;
     })
-    .join("");
-}
+    .join('');}
