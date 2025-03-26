acf23a1860ceedce4e82dc68c8493e4329b7199e
양시은 <mn07020@gmail.com>
2025-03-26 15:23:27 +0900

    FEAT : store todo 구현

--- js/store.js
+++ js/store.js
@@ -14,70 +14,73 @@
      }[]
   }
  */
-export const store = {
-  currentFunds: 0,
-
-  isFirstEdit: true,
-  todayId: 1,
-
-  dateList: [
-    {
-      id: 1,
-      date: new Date().toLocaleDateString(),
-    },
-  ],
-  detailList: {},
-};
-
-export function updateStorage() {
-  sessionStorage.setItem("store", JSON.stringify(store));
-}
-
-export function initStore() {
-  const storage = sessionStorage.getItem("store");
-  if (!storage) updateStorage();
-
-  const { dateList, detailList, todayId, currentFunds, isFirstEdit } =
-    JSON.parse(storage);
-
-  store.currentFunds = currentFunds;
-  store.isFirstEdit = isFirstEdit;
-  store.dateList = dateList;
-  store.detailList = detailList;
-  store.todayId = todayId;
-}
-
-export function addNewHistory(newHistory) {
-  try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList = null;
-    store.currentFunds = null;
-
-    updateStorage();
-    return true;
-  } catch (error) {
-    alert(error);
-    return false;
+  export const store = {
+    currentFunds: 0,
+  
+    isFirstEdit: true,
+    todayId: 1,
+  
+    dateList: [
+      {
+        id: 1,
+        date: new Date().toLocaleDateString(),
+      },
+    ],
+    detailList: {},
+  };
+  
+  export function updateStorage() {
+    sessionStorage.setItem('store', JSON.stringify(store));
   }
-}
-
-export function removeHistory(dateId, itemId) {
-  try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList[dateId] = null;
-
-    updateStorage();
-    return true;
-  } catch (error) {
-    alert(error);
-    return false;
+  
+  export function initStore() {
+    const storage = sessionStorage.getItem('store');
+    if (!storage) updateStorage();
+  
+    const { dateList, detailList, todayId, currentFunds, isFirstEdit } =
+      JSON.parse(storage);
+  
+    store.currentFunds = currentFunds;
+    store.isFirstEdit = isFirstEdit;
+    store.dateList = dateList;
+    store.detailList = detailList;
+    store.todayId = todayId;
   }
-}
+  
+  export function addNewHistory(newHistory) {
+    try {
+      if(store.detailList[store.todayId]){
+        store.detailList[store.todayId].push(newHistory);
+      } else {
+        store.detailList[store.todayId] = [newHistory];
+      }
+  
+      store.currentFunds -= newHistory.amount;
+  
+      updateStorage();
+      return true;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+  
+  export function removeHistory(dateId, itemId) {
+    try {
+      store.detailList[dateId] = store.detailList[dateId].filter(
+        ({ id, amount }) => {
+          if(id == Number(itemId)) {
+            store.currentFunds += amount;
+          }
+          return id !== Number(itemId);
+        }
+    );
+  
+      updateStorage();
+      return true;
+    } catch (error) {
+      alert(error);
+      return false;
+    }
+  }
+  
